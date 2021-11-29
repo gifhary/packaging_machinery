284d69411fc80e73f7aa59c09b3ba40e9aed82cc
gifhary <gifhary.1997@gmail.com>
2021-11-29 21:41:32 +0700

    feat: tabbar profile

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:packaging_machinery/widget/app_bar.dart';
+import 'package:packaging_machinery/widget/profile/my_account_tab.dart';
+import 'package:packaging_machinery/widget/profile/my_archive_tab.dart';
+import 'package:packaging_machinery/widget/profile/my_booking_tab.dart';
 
 enum ProfilePage { wallet, booking, archive, account }
 
@@ -11,62 +14,82 @@ class ProfileScreen extends StatelessWidget {
     double _height = MediaQuery.of(context).size.height;
     double _width = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(40),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: const Color.fromRGBO(46, 45, 42, 1),
-          title: const AppBarWidget(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(40),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: const Color.fromRGBO(46, 45, 42, 1),
+            title: const AppBarWidget(),
+          ),
         ),
-      ),
-      body: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Image.asset(
-            'assets/img/banner_1.png',
-            height: _height,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            height: _height,
-            width: double.infinity,
-            color: const Color.fromRGBO(0, 0, 0, 0.6),
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: _height,
-                  width: _width * 0.8,
-                  child: Column(
-                    children: [
-                      Container(
-                        color: const Color.fromRGBO(117, 111, 99, 1),
-                        height: 150,
-                      ),
-                      Expanded(
-                          child: Container(
-                        color: Colors.white,
-                      )),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: const Color.fromRGBO(117, 111, 99, 1),
-                  width: double.infinity,
-                  height: 43,
-                  child: const Center(
-                    child: Text(
-                      '©2022 by Samantha Tiara W. Master\'s Thesis Project - EM.',
-                      style: TextStyle(color: Colors.white),
+        body: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Image.asset(
+              'assets/img/banner_1.png',
+              height: _height,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              height: _height,
+              width: double.infinity,
+              color: const Color.fromRGBO(0, 0, 0, 0.6),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: _width * 0.8,
+                    child: Column(
+                      children: [
+                        Container(
+                          color: const Color.fromRGBO(117, 111, 99, 1),
+                          height: 150,
+                        ),
+                        const ColoredBox(
+                          color: Colors.white,
+                          child: TabBar(
+                            labelColor: Colors.black,
+                            indicatorColor: Color.fromRGBO(117, 111, 99, 1),
+                            tabs: [
+                              Tab(child: Text('My Bookings')),
+                              Tab(child: Text('My Archive')),
+                              Tab(child: Text('My Account')),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: _height,
+                          child: const TabBarView(
+                            children: [
+                              MyBookingTab(),
+                              MyArchiveTab(),
+                              MyAccountTab(),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                    color: const Color.fromRGBO(117, 111, 99, 1),
+                    width: double.infinity,
+                    height: 43,
+                    child: const Center(
+                      child: Text(
+                        '©2022 by Samantha Tiara W. Master\'s Thesis Project - EM.',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widget/profile/my_booking_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyBookingTab extends StatelessWidget {
+  const MyBookingTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white, child: Text('data'));
+  }
+}
