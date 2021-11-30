f1138adffff3eb918cabbc8aa1b321afee20274e
gifhary <gifhary.1997@gmail.com>
2021-11-30 21:44:21 +0700

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:packaging_machinery/route/app_route.dart';
 import 'package:packaging_machinery/route/route_constant.dart';
 
-Future<void> main() async {
+void main() async {
   await GetStorage.init();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
